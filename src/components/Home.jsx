99c976c4a53314f3c180c24926d78533f3ea7898
Aakash <vermaaakash1000@gmail.com>
2023-04-24 15:01:35 +0530

    Home page added

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../index.css'
+import bgImage from '../assets/bgImagePipe.png'
+import bgSpace from '../assets/bg-space.png'
+import LocationForm from './LocationForm'
+import BlockchainFeature from './BlockchainFeature'
+function Home() {
+
+    return (
+        <div className="bg-project-black flex flex-col h-screen home-screen">
+            <LocationForm />
+            <img src={bgImage} className=' w-full' alt="Bacground Image" />
+            <BlockchainFeature />
+            <img src={bgSpace} className=' w-full' alt="Bacground Image" />
+        </div>
+    )
+}
+
+export default Home
