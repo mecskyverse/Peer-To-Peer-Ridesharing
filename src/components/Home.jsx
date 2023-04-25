98796e305e6081d8680be5e273fbd1cc8921910e
Aakash <vermaaakash1000@gmail.com>
2023-04-25 11:27:25 +0530

    style object added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,30 @@ import bgSpace from '../assets/bg-space.png'
 import LocationForm from './LocationForm'
 import BlockchainFeature from './BlockchainFeature'
 function Home() {
+    const styles = {
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed'
+    }
+    const styles2 = {
+        backgroundImage: `url(${bgSpace})`,
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
+        // width: '100%',
+        // height: '100%'
+        overflow: 'auto'
 
+    }
     return (
-        <div className="bg-project-black flex flex-col h-screen home-screen">
-            <LocationForm />
-            <img src={bgImage} className=' w-full' alt="Bacground Image" />
-            <BlockchainFeature />
-            <img src={bgSpace} className=' w-full' alt="Bacground Image" />
-        </div>
+        <>
+            <div style={styles2} className="bg-project-black h-screen">
+                {/* <LocationForm /> */}
+                {/* <img src={bgImage} className=' w-full' alt="Bacground Image" /> */}
+                {/* <BlockchainFeature /> */}
+                {/* <img src={bgSpace} className=' w-full' alt="Bacground Image" /> */}
+            </div>
+            {/* <div style={styles} className='h-full w-full text-white'>helllo </div> */}
+        </>
     )
 }
 
