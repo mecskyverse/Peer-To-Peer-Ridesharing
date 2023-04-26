9f494571d2542a9fffe60b4af7b9ec5e33c536b7
Aakash <vermaaakash1000@gmail.com>
2023-04-26 10:36:17 +0530

    Layout updated

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './NavBar'
+import Footer from './Footer'
 function Layout() {
     return (
         <div className="site-wrapper">
             < Navbar />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
