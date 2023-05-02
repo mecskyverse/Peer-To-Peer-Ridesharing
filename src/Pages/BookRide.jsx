5939d5bce08a697fdddb9ecae773a4eff952bb97
Aakash <vermaaakash1000@gmail.com>
2023-05-02 19:44:57 +0530

    Maps Api added

--- src/Pages/BookRide.jsx
+++ src/Pages/BookRide.jsx
@@ -1,38 +1,97 @@
-import React from 'react'
-import { useJsApiLoader, GoogleMap } from '@react-google-maps/api'
-
+import React, { useState } from 'react'
+import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
+import { useForm } from 'react-hook-form'
 function BookRide() {
+    const [directionsResponse, setDirectionsResponse] = useState(null)
+    const [distance, setDistance] = useState('')
+    const [duration, setDuration] = useState('')
+    const originRef = React.useRef()
+    const destinationRef = React.useRef()
+
     const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+        libraries: ['places'],
     })
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+
+
+    if (!isLoaded)
+        return <div>Loading...</div>
+
+    // function to calculate Route
+    async function calculateRoute(data) {
+        console.log(data)
+        data.origin = originRef.current.value
+        data.destination = destinationRef.current.value
+        console.log(data)
+        console.log(originRef.current.value)
+        if (originRef.current.value === '' && destinationRef.current.value === '') {
+            return
+        }
+        const directionsService = new google.maps.DirectionsService()
+        const results = await directionsService.route({
+            origin: originRef.current.value,
+            destination: destinationRef.current.value,
+            travelMode: google.maps.TravelMode.DRIVING
+        })
+        setDirectionsResponse(results)
+        setDistance(results.routes[0].legs[0].distance.text)
+        setDuration(results.routes[0].legs[0].distance.text)
+    }
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     const center = { lat: 30.7506, lng: 76.6401 }
     return (
-
-
         <div className='ride-book h-full w-full flex'>
             <div className='text-xl w-1/2 bookride-background self-center'>
-                <form className='flex flex-col gap-10 pt-5 ml-20 text-white' action="">
+                <form onSubmit={handleSubmit(calculateRoute)} className='flex flex-col gap-10 pt-5 ml-20 text-white' action="">
                     <div className='flex flex-row justify-around'>
                         {/* //input field for location */}
-                        <input type="text" className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2 placeholder-form-text ' placeholder='Enter Firstname' />
+                        <input type="text" className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2 placeholder-form-text ' placeholder='Enter Firstname' name='firstName' {...register("firstName")} />
+
 
                         {/* //input field for destination */}
-                        <input type="text" className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2  placeholder-form-text ' placeholder='Enter LastNam' />
+                        <input
+                            type="text"
+                            className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2  placeholder-form-text '
+                            placeholder='Enter LastName'
+                            name='lastName'
+                            {...register("lastName")}
+                        />
 
                     </div>
                     <div className='flex flex-row justify-around'>
                         {/* //input field for location */}
-                        <input type="text" className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2 placeholder-form-text ' placeholder='Pickup Location' />
+                        <Autocomplete>
+                            <input
+                                type="text"
+                                className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2 placeholder-form-text '
+                                placeholder='Pickup Location'
+                                ref={originRef}
+                            />
+                        </Autocomplete>
 
                         {/* //input field for destination */}
-                        <input type="text" className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2  placeholder-form-text ' placeholder='Enter Destination' />
+                        <Autocomplete>
+                            <input type="text"
+                                className='w-48 h-14 bg-transparent border-2 border-sky-500 rounded-2xl p-2  placeholder-form-text ' placeholder='Enter Destination'
+                                name='destination'
+                                ref={destinationRef}
+
+
+                            />
+                        </Autocomplete>
                     </div>
 
-                    <input type="date" name="date" id="form-date" className='w-48 h-14 text-form-text bg-transparent border-2 border-[#f15a16] rounded-2xl p-2 self-center' style={{ filter: 'invert(1)' }} />
+                    <input type="date" name="date" id="form-date" className='w-48 h-14 text-form-text bg-transparent border-2 border-[#f15a16] rounded-2xl p-2 self-center' style={{ filter: 'invert(1)' }} {...register("date")} />
                     {/* //button to submit */}
-                    <button type='submit' className='h-12 self-center bg-transparent text-yellow-300 rounded-2xl p-2 justify-self-center border-2 border-sky-500'>Submit</button>
-
-
+                    <button type='submit' className='h-12 self-center bg-transparent text-yellow-300 rounded-2xl p-2 justify-self-center border-2 border-sky-500'>Get Estimate</button>
                 </form>
             </div>
             {/* Google Map */}
@@ -47,7 +106,11 @@ function BookRide() {
                         mapTypeControl: false,
                         fullscreenControl: false,
                     }}
-                />
+                >
+                    {console.log(directionsResponse)}
+                    {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+                    <Marker position={center} />
+                </GoogleMap>
             </div>
         </div >
     )
