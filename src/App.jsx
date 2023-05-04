a7b2e5607d38693a10b26e63a99a01140c20f6f3
Aakash <vermaaakash1000@gmail.com>
2023-05-04 22:54:22 +0530

    Host Ride link added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Layout from './Pages/Layout'
 import BookRide from './Pages/BookRide'
+import HostRide from './Pages/HostRide'
 // import { useMoralis } from "react-moralis"
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='bookRide' element={<BookRide />} />
+          <Route path='hostRide' element={<HostRide />} />
         </Route>
       </Routes>
     </BrowserRouter>
