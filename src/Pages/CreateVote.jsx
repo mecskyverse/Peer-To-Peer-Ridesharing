38ed8096b6a836a77443c3bcb6999a37306fb562
Aakash <vermaaakash1000@gmail.com>
2023-05-09 22:36:57 +0530

    Create Vote page added

--- src/Pages/CreateVote.jsx
+++ src/Pages/CreateVote.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import createVotebg from '../assets/createVote.jpg'
 function CreateVote() {
+    const [sidebar, setSidebar] = useState(false)
+    function handleClick() {
+        setSidebar(!sidebar)
+        console.log(sidebar)
+        console.log("clicked")
+    }
+    const showSideView = () => {
+        if (sidebar) {
+            // I have to Make a transition property later 
+            console.log("sidebar activated")
+            return (
+                <div className={`flex flex-col h-screen w-3/4 rounded-e-3xl text-white  fixed top-0 right-0 z-50 sidebar-background`}>
+                    <ul className='flex flex-col text-xl gap-10  mt-20 self-center ml-1/8 '>
+                        <li className="hover:animate-bounce ">Home</li>
+                        <li className="hover:animate-bounce ">Host Ride</li>
+                        <li className="hover:animate-bounce ">Book Ride</li>
+                        <li className="hover:animate-bounce ">Create Vote</li>
+                        <li className="hover:animate-bounce ">Connect Wallet</li>
+                    </ul>
+                </div>
+            )
+        }
+        else return null
+    }
     return (
-        <div className='create-vote text-white text-5xl '>CreateVote</div>
+
+
+        <>
+            <img src={createVotebg} alt="" className='create-vote h-full w-full absolute -z-0' />
+            {/* <div onClick={handleClick} className='create-vote absolute text-xl text-red-600 w-20 top-72 mt-2 left-40' >Click to see Current Votings</div> */}
+            <div className='relative  w-full h-full flex flex-col items-center justify-center top-16'>
+                <h1 className='font-serif text-[#1EFA22] text-3xl'>Create Vote For Fare</h1>
+                <p className='mt-10 text-lg font-serif text-sky-200'>A community Driven Fare management system where you can<br />
+                    choose the fare of your choice and vote for it. The fare with <br />
+                    the most votes
+                    will be the fare of the cab.
+                </p>
+                <p className='text-xl text-white mt-10'>Current Fare of Cab is /km</p>
+            </div>
+            <div className='absolute vote-list-button top-80 mt-3 left-28 ml-2'>
+                <button onClick={handleClick} className='hover:text-gray-300 vote-list-button bg-[#0E101C] text-white rounded-lg px-4 py-2'>Current Votings</button>
+            </div>
+            {showSideView()}
+        </>
+
+
     )
 }
 
