b23ea9dc55784a98270c8ac52c197c05aaf24cbd
Aakash <vermaaakash1000@gmail.com>
2023-05-21 23:01:18 +0530

    Booking Ride updated  for share Ride

--- src/Pages/BookingRide.jsx
+++ src/Pages/BookingRide.jsx
@@ -11,24 +11,34 @@ function BookingRide() {
 
     const { getProviderOrSigner, walletConnected } = useContext(MyContext);
     const [rideArray, setRideArray] = useState([]);
-
-
+    const [sameRideArray, setSameRideArray] = useState([]);
+    const [sharedRideArray, setSharedRideArray] = useState([]);
+    const [toggleSameRideArray, setToggleSameRideArray] = useState(false);
+    const [currentAddress, setCurrentAddress] = useState('');
     const location = useLocation()
 
     const [distance, setDistance] = useState(0)
     const [origin, setOrigin] = useState('')
     const [destination, setDestination] = useState('')
-    console.log(origin)
-    console.log(destination)
+    // console.log(origin)
+    // console.log(destination)
 
     useEffect(() => {
+        async function getUserAddress() {
+            const signer = await getProviderOrSigner(true);
+            const address = await signer.getAddress();
+            setCurrentAddress(address)
+
+        }
         availableRides();
         setDestination(location.state.destination)
         setOrigin(location.state.origin)
-        let currdistance = location.state.distance;
-        let roundedDistance = Math.round(parseFloat(currdistance)); // Round off the value
-        let wholeNumber = Math.floor(roundedDistance);
-        setDistance(wholeNumber)
+
+        setDistance(location.state.distance)
+        console.log("distance = " + distance)
+        getUserAddress();
+
+
     }, [distance, origin, destination])
 
 
@@ -68,70 +78,194 @@ function BookingRide() {
                 fare: ride.fare.toString(),
             }));
             setRideArray(formattedResult)
-            console.log("result = " + formattedResult[1].destination)
+
+            // console.log("result = " + formattedResult[1].destination)
         } catch (err) {
             console.error(err);
         }
     }
-    const showArray = () => {
-        if (rideArray.length === 0) {
+    const showRidesSameLocation = async () => {
+        // console.log("button clicked")
+        try {
+
+            const provider = await getProviderOrSigner();
+            const cutstomerContract = new Contract(
+                CUSTOMER_CONTRACT_ADDRESS,
+                CUSTOMER_CONTRACT_ABI,
+                provider
+            )
+            //calling the function to show the location which are having ride Location same as me
+            const result = await cutstomerContract.showRidesSameLocation(origin, destination);
+            const formattedResult = result.map((ride) => ({
+                ...ride,
+                distance: ride.distance.toString(),
+                fare: ride.fare.toString(),
+            }));
+            setSameRideArray(formattedResult)
+        } catch (err) { console.log(err) }
+
+    }
+    const getSharedRideArray = async () => {
+        try {
+
+            const provider = await getProviderOrSigner();
+            const cutstomerContract = new Contract(
+                CUSTOMER_CONTRACT_ADDRESS,
+                CUSTOMER_CONTRACT_ABI,
+                provider
+            )
+            //calling the function to show the location which are having ride Location same as me
+            const result = await cutstomerContract.showSharedRide();
+            console.log(result)
+            setSharedRideArray(result)
+        } catch (err) { console.log(err) }
+
+    }
+
+    const shareRideWithThis = async (address, origin, destination) => {
+        try {
+            const signer = await getProviderOrSigner(true);
+            const cutstomerContract = new Contract(
+                CUSTOMER_CONTRACT_ADDRESS,
+                CUSTOMER_CONTRACT_ABI,
+                signer
+            )
+
+            const tx = await cutstomerContract.shareBookRidefromlist(address, origin, destination);
+            await tx.wait();
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const showSharedRideArray = () => {
+        if (sharedRideArray.length === 0) {
             return (null)
         }
         else {
             return (
-                <table className='text-white '>
-                    <thead className='h-16'>
-                        <tr >
-                            <th className='w-80'>Address</th>
-                            <th>Origin</th>
-                            <th>Destination</th>
-                            <th>Distance</th>
-                            <th>Fare</th>
-                        </tr>
-                        <tr>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rideArray.map((ride, index) => (
-                            <React.Fragment key={index}>
-                                <tr key={index} className='h-8'>
-                                    <td className='text-center'>{ride.user}</td>
-                                    <td className='text-center'>{ride.origin}</td>
-                                    <td className='text-center'>{ride.destination}</td>
-                                    <td className='text-center'>{ride.distance}</td>
-                                    <td className='text-center'>{ride.fare} wei</td>
-                                </tr>
-                                <tr>
-                                    <td colSpan="5">
-                                        {/* Add a horizontal row here */}
-                                        <hr />
-                                    </td>
-                                </tr>
-                            </React.Fragment>
-                        ))}
-                    </tbody>
-                </table >
+                <>
+                    <div className='text-white text-3xl self-center mt-10 '>Shared Rides</div>
+                    {/* if (needSameRide) console.log("button clicked") */}
+                    <table className='text-white '>
+                        <thead className='h-16'>
+                            <tr >
+                                <th className='w-80'>Addresses</th>
+                                <th>Origin</th>
+                                <th>Destination</th>
+                            </tr>
+                            <tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {sharedRideArray.map((ride, index) => (
+                                <React.Fragment key={index}>
+                                    <tr key={index} className='h-8'>
+                                        <td className='text-center'>
+                                            {
+                                                ride.s_riders.map((rider, index) => (
+                                                    <div className='text-white'>{rider}</div>
+                                                ))
+                                            }
+                                        </td>
+                                        <td className='text-center'>{ride.origin}</td>
+                                        <td className='text-center'>{ride.destination}</td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan="3">
+                                            {/* Add a horizontal row here */}
+                                            <hr />
+                                        </td>
+                                    </tr>
+                                </React.Fragment>
+                            ))}
+                        </tbody>
+                    </table >
+                </>
             )
         }
-
-
     }
+
+    const showArray = (needSameRide) => {
+        if ((needSameRide ? sameRideArray : rideArray).length === 0) {
+            return (null)
+        }
+        else {
+            return (
+                <>
+                    <div className='text-white text-3xl self-center mt-10 '> {needSameRide ? 'Same Rides As Yours' : 'Available Rides'}</div>
+                    {/* if (needSameRide) console.log("button clicked") */}
+                    <table className='text-white '>
+                        <thead className='h-16'>
+                            <tr >
+                                <th className='w-80'>Address</th>
+                                <th>Origin</th>
+                                <th>Destination</th>
+                                <th>Distance</th>
+                                <th>Fare</th>
+                            </tr>
+                            <tr>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {(needSameRide ? sameRideArray : rideArray).map((ride, index) => (
+                                <React.Fragment key={index}>
+                                    <tr key={index} className='h-8'>
+                                        <td className='text-center'>{ride.user}</td>
+                                        <td className='text-center'>{ride.origin}</td>
+                                        <td className='text-center'>{ride.destination}</td>
+                                        <td className='text-center'>{ride.distance}</td>
+                                        <td className='text-center'>{ride.fare} wei</td>
+                                        {
+                                            ride.user.toString() != currentAddress.toString() && needSameRide ? (
+                                                <button
+                                                    onClick={() => shareRideWithThis(ride.user, ride.origin, ride.destination)}
+                                                    className="hover:bg-sky-400 text-red-600 border border-gray-400"
+                                                >
+                                                    share
+                                                </button>) : null
+
+                                        }
+                                    </tr>
+                                    <tr>
+                                        <td colSpan="6">
+                                            {/* Add a horizontal row here */}
+                                            <hr />
+                                        </td>
+                                    </tr>
+                                </React.Fragment>
+                            ))}
+                        </tbody>
+                    </table >
+                </>
+            )
+        }
+    }
+
 
 
 
 
     return (
-        <main className='ride-book w-full'>
+        <main className='ride-book w-full h-increase'>
             <section className='flex flex-col gap-10 w-full' >
-                <div className='text-white text-3xl self-center mt-10 '> Available Rides</div>
-                {showArray()}
+
+                {showArray(toggleSameRideArray)}
+                {showSharedRideArray()}
                 <div className='flex flex-row gap-8 self-center'>
                     <button onClick={addRider} className='hover:bg-sky-400 h-10 bg-slate-50 p-2 w-auto' >Create Ride</button>
-                    <button onClick={availableRides} className='hover:bg-sky-400 h-10 bg-slate-50    p-2 w-auto' >Available rides</button>
+                    <button onClick={() => {
+                        setToggleSameRideArray(false)
+                        availableRides()
+                    }} className='hover:bg-sky-400 h-10 bg-slate-50    p-2 w-auto' >Available rides</button>
+                    <button onClick={() => {
+                        setToggleSameRideArray(true)
+                        showRidesSameLocation()
+                    }} className='hover:bg-sky-400 h-10 bg-slate-50    p-2 w-auto' >Same Rides</button>
+                    <button onClick={getSharedRideArray} className='hover:bg-sky-400 h-10 bg-slate-50    p-2 w-auto' >Shared Rides</button>
                 </div>
             </section>
-        </main>
+        </main >
     )
 }
 
