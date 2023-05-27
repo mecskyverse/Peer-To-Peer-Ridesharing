b6b2e7a7c7dd633384a60e5f699e1cca84c7a1b0
Aakash <vermaaakash1000@gmail.com>
2023-05-27 17:47:57 +0530

    Blockchain Feature Section updated

--- src/Pages/BlockchainFeature.jsx
+++ src/Pages/BlockchainFeature.jsx
@@ -4,10 +4,10 @@ function BlockchainFeature() {
     return (
         <div className='flex md:flex-row flex-col w-full justify-between '>
             <img className='w-5/12 ml-10 hidden md:block' src={communitySvg} alt="community" />
-            <div className='md:w-1/3 w-full h-3/4 md:mt-10 mt-0 rounded-3xl md:rounded-s-3xl self-center bg-Blockchain-feature-background text-white '>
-                <h1 className='text-3xl text-center mt-5'>Features</h1>
-                <p className='p-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga pariatur fugit veniam saepe hic totam quia amet blanditiis ea quod enim voluptatum nemo eligendi aut fugiat, numquam animi? Architecto, distinctio! Aliquam ea asperiores, libero sit facere temporibus odit nulla aut! Ullam tenetur distinctio ipsa repellat pariatur! Beatae cum vero soluta.</p>
-                <p className='p-5 pt-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum pariatur est a totam alias aliquam explicabo assumenda dolor vitae odit iste, animi, iure recusandae nisi sit sunt optio labore quae laudantium beatae. Minus repudiandae cupiditate beatae, odio excepturi officiis, molestiae alias soluta voluptatem inventore in aliquam tenetur quasi nesciunt est?</p>
+            <div className='blockchain-feature md:w-1/3 w-full h-3/4 md:mt-10 mt-0 rounded-3xl md:rounded-s-3xl self-center bg-Blockchain-feature-background text-white '>
+                {/* <h1 className='text-3xl text-center mt-5'>Features</h1> */}
+                <p className='p-5 text-xl'>Peer-to-Peer Ridesharing is an attempt to solve the problem of centralized ridesharing platforms. RushGo helps you to be transparent yet secure as a driver or as a passenger. RushGo is built on the Ethereum Blockchain and secured by Smart Contracts. You can book your ride or share it with others. Not only that, but you can also create a vote for the fare price you want to set</p>
+                <p className='p-5 pt-0 text-xl '>Currently, RushGo is under development, but you can easily use features like Book Ride. Simply connect your Metamask Wallet with RushGo, and you're good to go.<b> This project is part of my 'learn while doing' method, which I've been using to learn to code. I have created it to practice my coding skills and as part of my projects.</b></p>
             </div>
         </div>
     )
